a93779e21c9ebb41e1e88f010a14d13c5312adb0
b1-loop <bojidarivanov98@gmail.com>
2025-11-03 13:01:12 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Travel_Journal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
